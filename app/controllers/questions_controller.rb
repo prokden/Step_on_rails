65a8b87e08551ad5934cc93f6eaead5c4a2bd1e3
prokden <prokden@gmail.com>
2022-10-11 12:11:18 +0300

    2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,12 @@ class QuestionsController < ApplicationController
     def index
         @questions = Question.all
     end
+
+    def new
+        @questions = Question.new
+    end
+
+    def create 
+        render plain: params
+    end
 end
